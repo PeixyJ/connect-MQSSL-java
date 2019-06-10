30700c3ab6b7107d8a28a7d15d3878859623b9c6
PeixyJ <peixy.j@gmail.com>
2019-06-10 09:46:29 +0800

    [init]java connect mq

---
+++ src/test/java/top/peixy/ibmmq/IbmMqApplicationTests.java
@@ -0,0 +1,16 @@
+package top.peixy.ibmmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbmMqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
